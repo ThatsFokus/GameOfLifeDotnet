01dbabc05a8d0a7629ff7a11e12e81f1d0c5fb12
ThatsFokus <fabi.gander@gmail.com>
2022-11-26 19:38:50 +0100

    Variable Cell size
    The Cell Size can now be configured in the Cell class

--- CircleOfLife.cs
+++ CircleOfLife.cs
@@ -13,7 +13,7 @@ public class CircleOfLife{
 		field  = new Cell[width, height];
 		for (int w = 0; w < width; w++){
 			for (int h = 0; h < height; h++){
-				field[w,h] = new Cell(w * 10, h * 10);
+				field[w,h] = new Cell(w * Cell.Size, h * Cell.Size);
 			}
 		}
 	}
